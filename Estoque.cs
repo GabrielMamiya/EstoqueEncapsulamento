6ac38856d663a82f71d3b30be5a1dfcc5e08563a
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-14 23:04:39 -0300

    Initial check-in of module EstoqueEncapsulamento

---
+++ Estoque.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace EstoqueEncapsulamento
+{
+    public class Estoque
+    {
+
+        public string nome { get; private set; }
+        public int estoque { get; private set; }
+
+        //constructor
+        public Estoque(string nome, int estoque)
+        {
+            this.nome = nome;
+            this.estoque = estoque;
+        }
+
+        public void imprimeEstoque(){
+            Console.WriteLine("Produto: " + nome);
+            Console.WriteLine("Valor no estoque: " + estoque);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstoqueEncapsulamento
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Estoque produto1;
+
+            string nome = Console.ReadLine();
+            int estoque = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            produto1 = new Estoque(nome, estoque);
+
+            produto1.imprimeEstoque();
+
+
+
+
+        }
+    }
+}
